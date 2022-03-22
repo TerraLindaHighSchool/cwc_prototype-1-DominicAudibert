ed4a1bcb35f71318b0c2d90c507285098667bbad
DominicAudibert <89489058+DominicAudibert@users.noreply.github.com>
2022-03-22 15:30:22 -0700

    Tutorial 1.1 Done

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private float turnSpeed = 45.0f;
+    private float speed = 20.0f;
+    private float horizontalInput;
+    private float forwardInput;
+   
+    void Update()
+    {
+        //Move the Vehicle Forward
+        horizontalInput = Input.GetAxis("Horizontal");
+            forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed *forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime * horizontalInput);
+    }
+}
